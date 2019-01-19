802c49c8d438710f45b8e9a6c23086df96b45852
Eran Krakovsky <ekrako@gmail.com>
2019-01-19 22:07:16 +0200

    added Map activity without api key (should be added for map to work)

--- app/src/main/java/info/krakovsky/memorableplaces/MainActivity.java
+++ app/src/main/java/info/krakovsky/memorableplaces/MainActivity.java
@@ -1,13 +1,21 @@
 package info.krakovsky.memorableplaces;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToMap(View view) {
+        Intent mapIntent = new Intent(getApplicationContext(),MapsActivity.class);
+        startActivity(mapIntent);
     }
 }
